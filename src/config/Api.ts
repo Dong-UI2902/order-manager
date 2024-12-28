ae8a2867966ba9612e2aba19df2d5f40ae72955b
Dong-UI2902 <dongnv2902@gmail.com>
2024-12-28 16:31:21 +0700

    update origin

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -3,12 +3,15 @@ import { isDev } from './helper';
 import { io } from "socket.io-client";
 
 export const HOST = isDev() ? 'http://localhost:5000' : 'https://order-server-nine.vercel.app';
-export const socket = io(HOST);
+export const socket = io(HOST, {
+  withCredentials: true,
+
+});
 
 //TODO: fix url
 export const axiosClient = axios.create({
   baseURL: isDev() ? 'http://localhost:5000/api' : 'https://order-server-nine.vercel.app/api',
-  // withCredentials: true
+  withCredentials: true
 });
 
 export const setCookie = (cookie: string) => {
